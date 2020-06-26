c58321eb4180ade5ab6f9b5521dacc1cac785175
AaronD07 <38016960+AaronD07@users.noreply.github.com>
2020-06-25 19:53:38 -0700

    Searching
    Binary, Interpolation, and Exponential

--- src/_01_Searching_Algorithms/_04_SearchTest.java
+++ src/_01_Searching_Algorithms/_04_SearchTest.java
@@ -22,16 +22,32 @@ class _04_SearchTest {
 	public void testBinarySearch() {
 		//2. use the assertEquals method to test your binary search method.
 		//   remember that the array must be sorted
+		int[] nums = {0, 1, 2, 3, 4, 5, 6};
+		assertEquals(1, _01_BinarySearch.binarySearch(nums, 0, nums.length-1, 1));
+		assertEquals(0, _01_BinarySearch.binarySearch(nums, 0, nums.length-1, 0));
+		assertEquals(-1, _01_BinarySearch.binarySearch(nums, 0, nums.length-1, 7));
+		assertEquals(6, _01_BinarySearch.binarySearch(nums, 0, nums.length-1, 6));
 	}
 	
 	@Test
 	public void testInterpolationSearch() {
 		//3. use the assertEquals method to test your interpolation search method.
 		//   remember that the array must be sorted and evenly distributed
+		int[] nums = {0, 1, 2, 3, 4, 5, 6};
+		assertEquals(1, _02_InterpolationSearch.interpolationSearch(nums, 1));
+		assertEquals(0, _02_InterpolationSearch.interpolationSearch(nums, 0));
+		assertEquals(-1, _02_InterpolationSearch.interpolationSearch(nums, 7));
+		assertEquals(6, _02_InterpolationSearch.interpolationSearch(nums, 6));
 	}
 	
 	@Test
 	public void testExponentialSearch() {
+		int[] nums = {0, 1, 2, 3, 4, 5, 6};
+		assertEquals(1, _03_ExponentialSearch.exponentialSearch(nums, 1));
+		assertEquals(0, _03_ExponentialSearch.exponentialSearch(nums, 0));
+		assertEquals(-1, _03_ExponentialSearch.exponentialSearch(nums, 7));
+		assertEquals(6, _03_ExponentialSearch.exponentialSearch(nums, 6));
+
 		//4. use the assertEquals method to test your exponential search method.
 		//   remember that the array must be sorted
 	}
--- src/_01_Searching_Algorithms/yes.java
+++
@@ -1,7 +0,0 @@
-package _01_Searching_Algorithms;
-
-public class yes {
-int mysteryMethod(String str) {
-	return str.length()*-1;
-}
-}
