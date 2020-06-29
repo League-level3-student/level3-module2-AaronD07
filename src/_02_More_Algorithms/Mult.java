88b87c5dd5143c3cc5a0375c735d71a9070ed609
AaronD07 <38016960+AaronD07@users.noreply.github.com>
2020-06-29 12:56:17 -0700

    :smile:

---
+++ src/_02_More_Algorithms/Mult.java
@@ -0,0 +1,9 @@
+package _02_More_Algorithms;
+
+public class Mult {
+	public static String Mult(int value, int value2){
+		
+		return value + "" + " * " + value2 +"" + " = " +value*value2;
+		
+	}
+}
